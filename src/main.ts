6082074e8f09090ebbe46121372373dd8c139903
qawatake <greenmackey@gmail.com>
2022-01-12 02:57:42 +0000

    catch event of trying search

--- src/main.ts
+++ src/main.ts
@@ -28,102 +28,81 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		this.registerEvent(
-			this.app.vault.on('create', (file: TAbstractFile) => {
-				if (file instanceof TFile) {
-					if (file.extension === 'png') {
-						console.log(file.name);
-						this.app.vault.create(
-							`info_of_${file.basename}.md`,
-							`ctime: ${file.stat.ctime}, mtime: ${file.stat.mtime}`
-						);
-					}
-				}
-			})
-		);
+		// this.registerEvent(
+		// 	this.app.vault.on('create', (file: TAbstractFile) => {
+		// 		if (file instanceof TFile) {
+		// 			if (file.extension === 'png') {
+		// 				console.log(file.name);
+		// 				this.app.vault.create(
+		// 					`info_of_${file.basename}.md`,
+		// 					`ctime: ${file.stat.ctime}, mtime: ${file.stat.mtime}`
+		// 				);
+		// 			}
+		// 		}
+		// 	})
+		// );
 
 		const app = this.app as AppExtension;
 		console.log(app);
 
-		const command = await this.waitUntilCommandsFound();
-		console.log(command);
+		// const command = await this.waitUntilCommandsFound();
+		// console.log(command);
 
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			'dice',
-			'Sample Plugin',
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				const app = this.app as AppExtension;
+		this.app.workspace.onLayoutReady(async () => {
+			// console.log(app.workspace.leftSplit);
+			// console.log(this.app.workspace.leftSplit.children);
+			this.hasFocusOnSearchInput();
+			console.log(this.findSearchLeaf());
+			const searchLeaf = this.findSearchLeaf();
+			const inputEl = searchLeaf?.querySelector(
+				'input[type="text"]'
+			) as HTMLInputElement;
+			console.log(inputEl);
+
+			this.registerDomEvent(inputEl, 'focus', () => {
 				console.log('a');
-				console.log(app);
-				console.log(app.commands.commands);
-				new Notice('This is a notice!');
-			}
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
+				console.log(this.hasFocusOnSearchInput());
+			});
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000)
-		);
 	}
 
 	onunload() {}
+
+	hasFocusOnSearchInput(): boolean {
+		const { containerEl } = this.app.workspace.leftSplit;
+		if (!(containerEl instanceof HTMLElement)) {
+			return false;
+		}
+		const inputEl = containerEl.querySelector(
+			'div.search-input-container > input'
+		);
+		if (inputEl === null) {
+			return false;
+		}
+		if (!document.hasFocus()) {
+			return false;
+		}
+		if (document.activeElement !== inputEl) {
+			console.log(document.activeElement);
+			return false;
+		}
+		return true;
+	}
+
+	findSearchLeaf(): HTMLElement | undefined {
+		const leafs = this.app.workspace.leftSplit.children[0].children as {
+			containerEl: HTMLElement;
+		}[];
+		return leafs.find((leaf) => {
+			const { containerEl } = leaf;
+			const inputEl = containerEl.querySelector(
+				'div.workspace-leaf-content[data-type="search"'
+			);
+			return inputEl !== null;
+		})?.containerEl;
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
