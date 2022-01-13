766669bfda27bc4d4e621a9ab9137a918fdffbf2
qawatake <greenmackey@gmail.com>
2022-01-13 03:25:14 +0000

    add searchAnalyzer (in progress)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Controller } from 'Controller';
 import { Plugin } from 'obsidian';
-import { searchAnalyzer } from 'searchAnalyzer';
+import { SearchAnalyzer, searchAnalyzer } from 'searchAnalyzer';
 import { findPropertyOwnerRecursively } from 'Util';
 import { AppExtension } from './uncover';
 
@@ -17,28 +17,44 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings | undefined;
 	controller: Controller | undefined;
+	searchAnalyzer: SearchAnalyzer | undefined;
 
 	override async onload() {
 		const app = this.app as AppExtension;
 
-		this.app.workspace.onLayoutReady(() => {
-			// console.log(new searchAnalyzer(this.app).getInputEl());
-		});
+		this.controller = new Controller(this.app, this).setup();
+		this.searchAnalyzer = new SearchAnalyzer(this.app);
+
+		// this.app.workspace.onLayoutReady(() => {
+		// 	console.log(new SearchAnalyzer(this.app).getSearchView());
+		// 	console.log(this.searchAnalyzer.collapseAll);
+		// });
+
+		// this.app.scope.register(['Ctrl'], '1', () => {
+		// 	this.searchAnalyzer?.getSearchView()?.setMatchingCase(true);
+		// });
+		// this.app.scope.register(['Ctrl'], '2', () => {
+		// 	this.searchAnalyzer?.getSearchView()?.setExplainSearch(true);
+		// });
+		// this.app.scope.register(['Ctrl'], '3', () => {
+		// 	this.searchAnalyzer?.getSearchView()?.setCollapseAll(true);
+		// });
+		// this.app.scope.register(['Ctrl'], '4', () => {
+		// 	this.searchAnalyzer.getSearchView()?.setExtraContext(true);
+		// });
+		// this.app.scope.register(['Ctrl'], '5', () => {
+		// 	this.searchAnalyzer.getSearchView()?.setSortOrder('byCreatedTime');
+		// });
+		// this.app.scope.register(['Ctrl'], '6', () => {
+		// 	this.searchAnalyzer.getSearchView()?.setSortOrder('byModifiedTime');
+		// });
 
 		console.log(app);
 		// console.log(findPropertyOwnerRecursively(app, 'setConfig'));
 		// console.log(findPropertyOwnerRecursively(app, 'sortOrder'));
-		console.log(this.app.vault.getConfig('fileSortOrder'));
+		// console.log(this.app.vault.getConfig('fileSortOrder'));
 
 		await this.loadSettings();
-
-		this.controller = new Controller(this.app, this).setup();
-
-		this.app.workspace.onLayoutReady(() => {
-			console.log(
-				(this.app.vault as any).getConfig('communityPluginSortOrder')
-			);
-		});
 	}
 
 	// onunload() {}
--- src/searchAnalyzer.ts
+++ src/searchAnalyzer.ts
@@ -1,6 +1,6 @@
 import { App, View, WorkspaceLeaf } from 'obsidian';
 
-export class searchAnalyzer {
+export class SearchAnalyzer {
 	app: App;
 
 	constructor(app: App) {
@@ -17,24 +17,131 @@ export class searchAnalyzer {
 	}
 
 	// getSearchInput():
+
+	getSearchView(): SearchView | undefined {
+		const leaf = this.getSearchLeaf();
+		if (!leaf) {
+			return undefined;
+		}
+
+		const view = leaf.view;
+		return isSearchView(view) ? view : undefined;
+	}
 }
 
 interface SearchView extends View {
-	setCollapseAll: (_collapseAll: boolean) => void;
+	matchingCase: boolean;
+	explainSearch: boolean;
 
-	setExplainSearch: (_explainSearch: boolean) => void;
+	setCollapseAll(collapseAll: boolean): void;
 
-	setExtraContext: (_extraContext: boolean) => void;
+	setExplainSearch(explainSearch: boolean): void;
 
-	setMatchingCase: (_matchingCase: boolean) => void;
+	setExtraContext(extraContext: boolean): void;
 
-	setSortOrder: (
-		_sortOrder:
+	setMatchingCase(matchingCase: boolean): void;
+
+	setSortOrder(
+		sortOrder:
 			| 'alphabeticalReverse'
 			| 'alphabetical'
 			| 'byModifiedTime'
 			| 'byModifiedTimeReverse'
 			| 'byCreatedTime'
 			| 'byCreatedTimeReverse'
-	) => void;
+	): void;
+
+	dom: SearchDom;
+}
+
+interface SearchDom {
+	extraContext: boolean;
+	collapseAll: boolean;
+	sortOrder: string;
+}
+
+type UnknownObject<T extends object> = {
+	[P in keyof T]: unknown;
+};
+
+function isSearchView(view: unknown): view is SearchView {
+	if (typeof view !== 'object') {
+		return false;
+	}
+	if (view === null) {
+		return false;
+	}
+
+	const {
+		matchingCase,
+		explainSearch,
+		dom,
+		setCollapseAll,
+		setExplainSearch,
+		setExtraContext,
+		setMatchingCase,
+		setSortOrder,
+	} = view as UnknownObject<SearchView>;
+
+	if (typeof matchingCase !== 'boolean') {
+		return false;
+	}
+	if (typeof explainSearch !== 'boolean') {
+		return false;
+	}
+	if (!isSearchDom(dom)) {
+		return false;
+	}
+	if (!(setCollapseAll instanceof Function)) {
+		return false;
+	}
+	if (!(setExplainSearch instanceof Function)) {
+		return false;
+	}
+	if (!(setExtraContext instanceof Function)) {
+		return false;
+	}
+	if (!(setMatchingCase instanceof Function)) {
+		return false;
+	}
+	if (!(setSortOrder instanceof Function)) {
+		return false;
+	}
+
+	return true;
+}
+
+function isSearchDom(obj: unknown): obj is SearchDom {
+	if (typeof obj !== 'object') {
+		return false;
+	}
+	if (obj === null) {
+		return false;
+	}
+
+	const { extraContext, collapseAll, sortOrder } =
+		obj as UnknownObject<SearchDom>;
+
+	if (typeof extraContext !== 'boolean') {
+		return false;
+	}
+	if (typeof collapseAll !== 'boolean') {
+		return false;
+	}
+	if (typeof sortOrder !== 'string') {
+		return false;
+	}
+	if (
+		![
+			'alphabeticalReverse',
+			'alphabetical',
+			'byModifiedTime',
+			'byModifiedTimeReverse',
+			'byCreatedTime',
+			'byCreatedTimeReverse',
+		].includes(sortOrder)
+	) {
+		return false;
+	}
+	return true;
 }
