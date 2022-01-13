c76f148d41b6da53c05f1ec4026b2d3e3a892b63
qawatake <greenmackey@gmail.com>
2022-01-13 02:13:41 +0000

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,28 +4,29 @@ module.exports = {
 		es2021: true,
 	},
 	extends: [
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:@typescript-eslint/recommended",
-		"prettier",
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
 	],
-	parser: "@typescript-eslint/parser",
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true,
 		},
 		ecmaVersion: 13,
-		sourceType: "module",
+		sourceType: 'module',
 	},
-	plugins: ["react", "@typescript-eslint"],
+	plugins: ['react', '@typescript-eslint'],
 	rules: {
-		"no-console": process.env.NODE_ENV === "production" ? 2 : 0,
-		"@typescript-eslint/explicit-module-boundary-types": 0,
-		"no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+		'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+		'@typescript-eslint/explicit-module-boundary-types': 0,
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['error'],
 	},
 	settings: {
 		react: {
-			version: "detect",
+			version: 'detect',
 		},
 	},
 };
