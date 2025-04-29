aa713e5a2b2bae8281419ea86adeb3d36e5ab9dd
qawatake <38106890+qawatake@users.noreply.github.com>
2025-04-29 15:37:41 +0900

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,21 +2,29 @@ import { defineConfig, devices } from "@playwright/test";
 
 // See https://playwright.dev/docs/test-configuration.
 export default defineConfig({
-	fullyParallel: false,
-	// forbidOnly: !!process.env["CI"],
-	use: {
-		trace: "retain-on-failure",
-	},
-	projects: [
-		{
-			name: "e2e",
-			testDir: "./tests/e2e",
-		},
-		{
-			name: "e2e-setup",
-			testDir: "./tests/e2e-setup",
-			testMatch: "**/*.ts",
-		},
-	],
-	timeout: 300 * 1000,
+  fullyParallel: false,
+  // forbidOnly: !!process.env["CI"],
+  use: {
+    trace: "retain-on-failure",
+  },
+  projects: [
+    {
+      name: "e2e",
+      testDir: "./tests/e2e",
+      use: {
+        browserName: "chromium",
+        headless: false,
+      },
+    },
+    {
+      name: "e2e-setup",
+      testDir: "./tests/e2e-setup",
+      testMatch: "**/*.ts",
+      use: {
+        browserName: "chromium",
+        headless: false,
+      },
+    },
+  ],
+  timeout: 300 * 1000,
 });
